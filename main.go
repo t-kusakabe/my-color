5f2d115ec2e623848637ad4b56869ed83799b82e
t-kusakabe <ttmac0704@gmail.com>
2018-03-12 18:31:51 +0900

    Create Scraping

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"code.google.com/p/go.net/html"
+  "fmt"
+  "io"
 )
 
-func main() {
+func Scraping(url string) []Result {
+  res, err := http.Get(url)
+  if err != nil {
+    fmt.Println(err)
+  }
 
+  defer res.BodyClose()
+  results := ParseItem(res.Body)
+
+  return results
+}
+
+func main() {
+  url := "https://github.com/users/t-kusakabe/contributions"
+  dom := Scraping(url)
 }
